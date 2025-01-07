4111d06fe51a5a0cc0de30d44fab5deacb779069
Arkady <144838822+arkadymarch@users.noreply.github.com>
2025-01-07 00:10:21 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const searchRouter = require("./src/api/search");
 
 app.use("/api", searchRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });

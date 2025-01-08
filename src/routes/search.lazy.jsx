86653942d8a9063eb063d007cce10b585c012e59
Arkady <144838822+arkadymarch@users.noreply.github.com>
2025-01-07 20:17:16 -0600

    cart functionality

--- src/routes/search.lazy.jsx
+++ src/routes/search.lazy.jsx
@@ -12,7 +12,7 @@ function RouteComponent() {
     const [results, setResults] = useState([])
     const [page, setPage] = useState(0)
     const [loading, setLoading] = useState(true)
-
+    const [list, setList] = useState([])
 
     async function fetchAnime() {
         // setLoading(true) 
@@ -35,38 +35,38 @@ function RouteComponent() {
   return (
     <div>
         <div>
-            <input type='text' placeholder='Search Anime' value={searchTerm} onChange={
-                (e) => {
-                    setSearchTerm(e.target.value)
-                }
-            } />
+            <input type='text' placeholder='Search Anime' value={searchTerm} 
+            onChange={(e) => {setSearchTerm(e.target.value)}} />
         </div>
         <div>
-            {
-            loading ? <div>Loading...</div> : results.map((anime, index) => (<div key={index}>{anime.title}</div>))
-            }
+            {loading ? <div>Loading...</div> : results.map((anime, index) => (<div key={index}>
+                {anime.title} 
+                <label>
+                    <input type="checkbox" id={anime.title}
+                    onChange={
+                        (e) => {
+                            if (e.target.checked)
+                                setList([...list, anime.title])
+                            else 
+                                setList(list.filter(title => title !== anime.title))
+                        }
+                    }
+                    checked={list.includes(anime.title)}
+                    />
+                    Add
+                </label>
+            </div>))}
         </div>
         <div>
             <button
-            onClick={() => {
-                if (page > 0) {
-                    setPage(page - 1)
-                }
-            }}
-            disabled={page === 0}
-            >
-                Previous
-            </button>
-            <button
-            onClick={() => setPage(page + 1)}
-            >
-                Next
-            </button>
-
-
-            <div>
-                Page {page + 1}
-            </div>
+                onClick={() => {if (page > 0) {setPage(page - 1)}}}
+                disabled={page === 0}
+            >Previous</button>
+            <button onClick={() => setPage(page + 1)}>Next</button>
+            <div>Page {page + 1}</div>
+        </div>
+        <div>
+            List: {list}
         </div>
 
     </div>

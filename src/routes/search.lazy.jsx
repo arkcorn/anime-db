d108c0b92458e869fb3f517c7a269613ea425796
Arkady <144838822+arkadymarch@users.noreply.github.com>
2025-01-08 21:20:29 -0600

    add tailwind, update search styles

--- src/routes/search.lazy.jsx
+++ src/routes/search.lazy.jsx
@@ -40,15 +40,19 @@ function RouteComponent() {
             <Link to="/">Home</Link>
         </div>  
         <div>
-            <input type='text' placeholder='Search Anime' value={searchTerm} 
+            <input type='text' placeholder='Search Anime' value={searchTerm} className='border-black border-2 block mx-auto rounded' 
             onChange={(e) => {setSearchTerm(e.target.value)}} />
         </div>
-        <div>
-            {loading ? <div>Loading...</div> : results.map((anime, index) => (<div key={index}>
-                <img src={anime.thumbnail}/>
-                <Link to="/anime/$title" params={anime}>{anime.title} </Link>
-                <label>
-                    <input type="checkbox" id={anime.title}
+        <div className='w-8/12 mx-auto'>
+            {loading ? <div>Loading...</div> : results.map((anime, index) => (<Link to="/anime/$title" key={index} params={anime} className='search-elem bg-blue-200 rounded border-blue-600 m-0.5 block'>
+                <img src={anime.thumbnail} className='inline-block p-1'/>
+                <Link to="/anime/$title" params={anime} className='text-blue-700'>{anime.title} </Link>
+                <label
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+                >
+                    <input type="checkbox" id={anime.title} className='m-2'
                     onChange={
                         (e) => {
                             if (e.target.checked)
@@ -59,15 +63,15 @@ function RouteComponent() {
                     }
                     checked={list.includes(anime.title)}
                     />
-                    Add
+                     Add
                 </label>
-            </div>))}
+            </Link>))}
         </div>
         <div>
             <button
                 onClick={() => {if (page > 0) {setPage(page - 1)}}}
                 disabled={page === 0}
-            >Previous</button>
+            >Previous</button> 
             <button onClick={() => setPage(page + 1)}>Next</button>
             <div>Page {page + 1}</div>
         </div>
